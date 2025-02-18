52c074dfbb2df473ddb4cb250510adf68ebc94cf
seoyoung <1203sylim@gmail.com>
2025-02-18 14:34:16 +0900

    update

---
+++ myPackage/math/fomula.py
@@ -0,0 +1,14 @@
+pi = 3.14
+
+def my_max(a, b):
+    if a > b:
+        return s
+    else:
+        return b
+
+
+def fact(n):
+    if n == 1:
+        return 1
+    else:
+        return fact(n-1) * n
